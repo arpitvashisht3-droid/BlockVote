7e9914d2d482332afacaa5da73a9467b98d6b69a
divyam <divyamkansal07@gmail.com>
2026-09-03 23:49:12 +0530

    Move BlockVote project to blockchain branch

--- ignition/modules/blockvote.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const BlockVoteModule = buildModule("BlockVoteModule", (m) => {
-  const blockVote = m.contract("BlockVote");
-
-  return { blockVote };
-});
-
-export default BlockVoteModule;
