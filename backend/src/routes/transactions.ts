de3989b873e972864a876aef7d7fe96c37ce739f
Mayank Sharma <109mayanksharma@gmail.com>
2026-09-05 15:05:02 +0530

    Revert "complete BlockVote integration and demo auth"
    This reverts commit 0df24c36388f7168ec6fc5ab2bd767a258bfb7ac.

--- backend/src/routes/transactions.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { electionController } from '../controllers/electionController';
-import { validateVoteTransaction } from '../middleware/validate';
-
-const router = Router();
-
-router.post('/vote', validateVoteTransaction, (req, res, next) => electionController.castVote(req, res, next));
-
-export default router;
--- blockchain/scripts/deploy.ts
+++
@@ -1,8 +0,0 @@
-import { network } from "hardhat";
-
-const { viem } = await network.create();
-
-const blockVote = await viem.deployContract("BlockVote");
-
-console.log("BlockVote deployed successfully!");
-console.log("Contract address:", blockVote.address);
